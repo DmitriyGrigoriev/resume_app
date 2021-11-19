f8677aec39cccf239865abfaee0716d51d6c48cf
Dmitriy Grigorev <dimag@inbox.ru>
2021-11-19 12:32:20 +0300

    fix: Change url for home page

--- main/urls.py
+++ main/urls.py
@@ -6,9 +6,9 @@ app_name = "main"
 
 urlpatterns = [
 	path('resume/', views.IndexView.as_view(), name="home"),
-	path('contact/', views.ContactView.as_view(), name="contact"),
-	path('portfolio/', views.PortfolioView.as_view(), name="portfolios"),
-	path('portfolio/<slug:slug>', views.PortfolioDetailView.as_view(), name="portfolio"),
-	path('blog/', views.BlogView.as_view(), name="blogs"),
-	path('blog/<slug:slug>', views.BlogDetailView.as_view(), name="blog"),
+	path('resume/contact/', views.ContactView.as_view(), name="contact"),
+	path('resume/portfolio/', views.PortfolioView.as_view(), name="portfolios"),
+	path('resume/portfolio/<slug:slug>', views.PortfolioDetailView.as_view(), name="portfolio"),
+	path('resume/blog/', views.BlogView.as_view(), name="blogs"),
+	path('resume/blog/<slug:slug>', views.BlogDetailView.as_view(), name="blog"),
 	]
